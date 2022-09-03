c7847898c90783e4d6db9137d9d73dae5e1b3401
Thesacraft <91694323+Thesacraft@users.noreply.github.com>
2022-09-03 19:20:13 +0200

    GUI YEAAAA

--- UpdaterLib.py
+++ UpdaterLib.py
@@ -2,12 +2,16 @@ import json
 import os.path
 import subprocess
 import tkinter.messagebox
+from tkinter import *
+from tkinter.ttk import *
 import zipfile
 import shutil
-
+import threading
+import urllib.request
 import requests
+import time
 
-class Updater():
+class TestUpdater():
     def __init__(self):
         self.createvars()
     def createvars(self):
@@ -22,13 +26,29 @@ class Updater():
         self.Update = False
         self.version = 0.0
         self.gamefile = ""
-        self.standardConfig = {"info":"U only need the id!","version":"https://drive.google.com/uc?export=download&id=","game":"https://drive.google.com/uc?export=download&id=","ForceUpdate":False,"gamefilename":"example.exe","gamefilepath":"example/example.exe","requireupdate":False,"gamename":"example"}
+        self.standardConfig = {"info":"You only need the id!","version":"https://drive.google.com/uc?export=download&id=","game":"https://drive.google.com/uc?export=download&id=","ForceUpdate":False,"gamefilename":"example.exe","gamefilepath":"example/example.exe","requireupdate":False,"gamename":"example"}
     def run(self):
         self.readConfig()
+    def createGUI(self):
+        self.root = Tk()
+        self.root.title(f"{self.gamename} Updater")
+        self.Label = Label(self.root,text="Checking for Updates")
+        self.Label.pack(pady=10)
+        self.progress = Progressbar(self.root,orient=HORIZONTAL,length=200,mode="determinate")
+        self.progress.pack(expand=True)
+        self.progress["value"] = 0
+        self.root.mainloop()
+
     def readConfig(self):
         if not os.path.exists("config.json"):
             with open("config.json","w+") as jsonFile:
                 jsonFile.write(json.dumps(self.standardConfig))
+        with open("config.json","r") as fh:
+            if json.load(fh) == self.standardConfig:
+                root = Tk()
+                root.withdraw()
+                tkinter.messagebox.showerror(title="Updater", message="You must configure the config first!")
+                exit()
         if not os.path.exists("version.txt"):
             with open("version.txt","w+") as fh:
                 fh.write("0.0")
@@ -42,54 +62,86 @@ class Updater():
             self.gamefilename = jsonObject["gamefilename"]
             self.versionlink = jsonObject["version"]
             self.forceUpdate = jsonObject["ForceUpdate"]
+
+
         with open("version.txt","r") as fh:
             self.version = fh.read()
-        self.checkforUpdate()
+        #self.checkforUpdate()
+        new_thread = threading.Thread(target=self.checkforUpdate)
+        new_thread.daemon = True
+        new_thread.start()
+        self.createGUI()
     def checkforUpdate(self):
         online_version = requests.get(self.versionlink).text
         self.new_version = online_version
         if not self.version == online_version:
             print("Update Available")
+            self.Label["text"] = "Update Available"
             if self.forceUpdate == True:
-                self.update(online_version)
+                self.update()
+
             else:
-                root = tkinter.Tk()
-                root.withdraw()
                 response = tkinter.messagebox.askquestion(title=f"{self.gamename} Updater", message=f"The Update({online_version}) of {self.gamename} is available, would you like to update?")#, **options)
-                root.destroy()
                 if response == "yes":
-                    self.update(online_version)
+                    self.update()
+
                 else:
                     if self.require == False:
                         self.startGame()
+                    self.root.destroy()
         else:
             self.startGame()
     def extract(self):
         print("extracting...")
+        self.Label["text"] = "extracting"
         with zipfile.ZipFile("game.zip","r")as zip_ref:
             zip_ref.extractall("Application_new")
+        self.progress["value"] = 90
         os.remove("game.zip")
         self.updateGame()
     def updateGame(self):
+        self.Label["text"] = "Copying Files"
         if os.path.exists("Application"):
             shutil.rmtree("Application")
         #os.makedirs("Application")
         working_path = os.getcwd()
         print(working_path)
+        self.progress["value"] = 95
+
         shutil.copytree(r"Application_new",r"Application")
+        self.Label["text"] = "Cleaning Up"
         shutil.rmtree("Application_new")
         with open("version.txt","w") as fh:
             fh.write(str(self.new_version))
+        self.progress["value"] = 100
         self.startGame()
     def startGame(self):
         subprocess.Popen(self.gamefilecwd+"\\"+self.gamefilename,cwd=self.gamefilecwd)
-    def update(self,version):
+        time.sleep(0.3)
+        self.root.destroy()
+    def fileSize(self):
+        i = os.path.getsize('game.zip')
+        i_before = 0
+        while i < self.file_size:
+            if i_before < i:
+                inter = (self.file_size/i)
+                step = 100.0 -inter
+                step = step*0.5
+                self.progress["value"] = step
+            i = os.path.getsize('game.zip')
+    def update(self,version=True):
+        print(self.new_version)
+        self.Label["text"] = f"Downloading Update v{self.new_version}"
         if os.path.exists("Application_new"):
             shutil.rmtree("Application_new")
         os.makedirs("Application_new")
         response = requests.get(self.gamelink)
-
+        file = urllib.request.urlopen(self.gamelink)
+        self.file_size=file.length
         with open("game.zip","wb") as handle:
+            thread = threading.Thread(target=self.fileSize)
+            thread.daemon = True
+            thread.start()
             for data in response.iter_content(chunk_size=1024):
                 if data:
                     handle.write(data)
